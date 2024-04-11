ac40f4a2b62b80e4f6fc26c51d86553b2d1f0e88
Dina Zhuzhleva <106472944+DinaGala@users.noreply.github.com>
2024-04-11 20:32:20 +0200

    cpp02 ex01 done

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nzhuzhle <nzhuzhle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 18:27:46 by nzhuzhle          #+#    #+#             */
+/*   Updated: 2024/04/11 20:29:19 by nzhuzhle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed &other);
+		Fixed& operator=(const Fixed &other);
+		~Fixed();
+		
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		
+	private:
+		int					_value;
+		static const int	_frac = 8;
+};
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &val);
+#endif
